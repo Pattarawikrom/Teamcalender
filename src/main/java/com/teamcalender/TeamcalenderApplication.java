9ae35ad4023878dc05fab953f3560a6e8025c105
Patti <p.bredow@campus.tu-berlin.de>
2022-09-26 15:41:06 +0200

    init commit

---
+++ src/main/java/com/teamcalender/TeamcalenderApplication.java
@@ -0,0 +1,13 @@
+package com.teamcalender;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamcalenderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TeamcalenderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/teamcalender/TeamcalenderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.teamcalender;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamcalenderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
